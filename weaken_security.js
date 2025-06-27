aa67aa1f061dbbbd6a91c65c8a8284322ddfc43f
Hotakus <hotakus@foxmail.com>
2025-06-28 03:05:21 +0800

    Update weaken_security.js

--- weaken_security.js
+++ weaken_security.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
     // let server_name = ns.args[0]
-    let server_list = ["foodnstuff", "sigma-cosmetics", "joesguns", "hong-fang-tea", "harakiri-sushi"]
+    let server_list = ["foodnstuff", "sigma-cosmetics", "joesguns", "hong-fang-tea", "harakiri-sushi", "iron-gym"]
 
     for (var i = 0; i < server_list.length; i++) {
         while (await ns.getServerSecurityLevel(server_list[i]) > await ns.getServerMinSecurityLevel(server_list[i])) {
